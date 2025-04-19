a85f429c22f4dca7d7d697d0552920029a84163c
Tassochaves <tassogt@hotmail.com>
2025-04-18 21:33:22 -0300

    Feat: create user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { SigninDTO, SignupDTO } from './dtos/auth';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  signin(data: SigninDTO) {
-    return data;
+  constructor(private prismaService: PrismaService) {}
+
+  async signup(data: SignupDTO) {
+    const userAlreadyExists = await this.prismaService.user.findUnique({
+      where: { email: data.email },
+    });
+
+    if (userAlreadyExists) {
+      throw new UnauthorizedException('User already exists');
+    }
+
+    const user = await this.prismaService.user.create({ data });
+
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    };
   }
 
-  signup(data: SignupDTO) {
+  signin(data: SigninDTO) {
     return data;
   }
 }
